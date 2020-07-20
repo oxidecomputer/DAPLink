d06d3c198fba6f6f75c5442c4e8341de8385831d
Rick Altherr <rick@oxidecomputer.com>
2020-07-20 10:44:09 -0700

    lpc4322: Enable SWO on UART1

--- source/hic_hal/nxp/lpc4322/DAP_config.h
+++ source/hic_hal/nxp/lpc4322/DAP_config.h
@@ -93,7 +93,7 @@ typedef unsigned int    BOOL;
 
 /// Indicate that UART Serial Wire Output (SWO) trace is available.
 /// This information is returned by the command \ref DAP_Info as part of <b>Capabilities</b>.
-#define SWO_UART                0               ///< SWO UART:  1 = available, 0 = not available
+#define SWO_UART                1               ///< SWO UART:  1 = available, 0 = not available
 
 /// Maximum SWO UART Baudrate
 #define SWO_UART_MAX_BAUDRATE   10000000U       ///< SWO UART Maximum Baudrate in Hz
@@ -105,6 +105,8 @@ typedef unsigned int    BOOL;
 /// SWO Trace Buffer Size.
 #define SWO_BUFFER_SIZE         4096U           ///< SWO Trace Buffer Size in bytes (must be 2^n)
 
+/// UART device used to receive SWO
+#define SWO_USART_PORT          1               ///< UART1 is used for the SWO UART.
 
 /// Debug Unit is connected to fixed Target Device.
 /// The Debug Unit may be part of an evaluation board and always connected to a fixed
